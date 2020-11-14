8d77e472d4638f2e4fcbd214015d22e291eb67a9
aronb <aron.buffel@student.howest.be>
2020-11-14 15:12:50 +0100

    commit 8 make js functions for mockdata in dedicated file

---
+++ src/assets/js/mockData.js
@@ -0,0 +1,53 @@
+function getUserOrdersMock() {
+    return [
+            {
+                orderId : "1",
+                userId: "Jan jansens",
+                rocketId: 456464,
+                statusId: 4,
+                mass: 6,
+                width: 2,
+                height: 2,
+                depth: 400,
+                price: 600
+            },
+            {
+                orderId: 2,
+                userId: "Jan jansens",
+                rocketId: 666664,
+                statusId: 2,
+                mass: 2,
+                width: 2,
+                height: 2,
+                depth: 400,
+                price: 200
+            },
+            {
+                orderId: 3,
+                userId: "Jan jansens",
+                rocketId: 754874,
+                statusId: 1,
+                mass: 2,
+                width: 2,
+                height: 2,
+                depth: 400,
+                price: 600
+            },
+        ]
+}
+
+function getUserOrderMock() {
+    return {
+        order: {
+            orderId: "1",
+            userId: "Jan jansens",
+            rocketId: 456464,
+            statusId: 4,
+            mass: 6,
+            width: 2,
+            height: 2,
+            depth: 400,
+            price: 600
+        }
+    };
+}
--- src/assets/js/userOrder.js
+++ src/assets/js/userOrder.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", userOrderInit);
-let tableHeaders = "<tr class=\"tableHeaders\"><th scope=\"col\">From:</th><th scope=\"col\">To:</th><th scope=\"col\">Address:</th><th scope=\"col\">Ordernr:</th><th scope=\"col\">Orderstatus:</th><th scope=\"col\">Departure:</th><th scope=\"col\">Arrival:</th><th scope=\"col\">Price:</th><td></td></tr>";
+const tableHeaders = "<tr class=\"tableHeaders\"><th scope=\"col\">From:</th><th scope=\"col\">To:</th><th scope=\"col\">Address:</th><th scope=\"col\">Ordernr:</th><th scope=\"col\">Orderstatus:</th><th scope=\"col\">Departure:</th><th scope=\"col\">Arrival:</th><th scope=\"col\">Price:</th><td></td></tr>";
 
 function userOrderInit(){
     loadInOrders();
@@ -7,60 +7,26 @@ function userOrderInit(){
 }
 
 function loadInOrders() {
-
-
-    let ordersMock = {
-        "orders": [
-            {
-                "orderId": "1",
-                "userId": "Jan jansens",
-                "rocketId": 456464,
-                "statusId": 4,
-                "mass": 6,
-                "width": 2,
-                "height": 2,
-                "depth": 400,
-                "price": 600
-            },
-            {
-                "orderId": 2,
-                "userId": "Jan jansens",
-                "rocketId": 666664,
-                "statusId": 2,
-                "mass": 2,
-                "width": 2,
-                "height": 2,
-                "depth": 400,
-                "price": 200
-            },
-            {
-                "orderId": 3,
-                "userId": "Jan jansens",
-                "rocketId": 754874,
-                "statusId": 1,
-                "mass": 2,
-                "width": 2,
-                "height": 2,
-                "depth": 400,
-                "price": 600
-            },
-        ]
-    };
-
-
-
-    showOrders(ordersMock.orders);
-
+    let orders = getUserOrdersMock();
+    //spit
+    showOrdersToEarth(orders);
+    showOrdersToMars(orders);
 }
 
-function showOrders(orders) {
-    let orderRows = "";
+function showOrdersToEarth(orders) {
+    let orderRowsToEarth ="";
     for(let i in orders){
-        orderRows += "<tr data-row='" + orders[i].orderId + "'><td>"+orders[i].userId+"</td><td>NAME RECIEVER</td><td>ADDRESS RECIEVER</td><td>"+orders[i].orderId+"</td><td>"+orders[i].statusId+"</td><td>"+orders[i].rocketId+"</td><td>"+orders[i].rocketId+"</td><td>"+orders[i].price+"</td><td><button>view more</button></td></tr>";
-        console.log(orders);
-        console.log(orderRows);
-
+        orderRowsToEarth += "<tr data-row='" + orders[i].orderId + "'><td>"+orders[i].userId+"</td><td>NAME RECIEVER</td><td>ADDRESS RECIEVER</td><td>"+orders[i].orderId+"</td><td>"+orders[i].statusId+"</td><td>"+orders[i].rocketId+"</td><td>"+orders[i].rocketId+"</td><td>"+orders[i].price+"</td><td><button>view more</button></td></tr>";
+        console.log(orderRowsToEarth);
     }
-    document.getElementById("flightsToEarthContent").innerHTML = tableHeaders + orderRows;
+    document.getElementById("flightsToEarthContent").innerHTML = tableHeaders + orderRowsToEarth;
+}
 
+function showOrdersToMars(orders){
+    let orderRowsToMars="";
+    for(let i in orders){
+        orderRowsToMars += "<tr data-row='" + orders[i].orderId + "'><td>"+orders[i].userId+"</td><td>NAME RECIEVER</td><td>ADDRESS RECIEVER</td><td>"+orders[i].orderId+"</td><td>"+orders[i].statusId+"</td><td>"+orders[i].rocketId+"</td><td>"+orders[i].rocketId+"</td><td>"+orders[i].price+"</td><td><button>view more</button></td></tr>";
+        console.log(orderRowsToMars);
+    }
+    document.getElementById("flightsToEarthContent").innerHTML = tableHeaders + orderRowsToMars;
 }
