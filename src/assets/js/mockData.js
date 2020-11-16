09abecf01580c8519c2ca6e74786373d49f26e6b
Thomas <thomas.van.nevel@student.howest.be>
2020-11-16 21:48:11 +0100

    refactored userOrder.js

--- src/assets/js/mockData.js
+++ src/assets/js/mockData.js
@@ -1,41 +1,53 @@
-const MOCK_DATA = [
-            {
-                orderId : "1",
-                userId: "Jan jansens",
-                rocketId: 456464,
-                statusId: 4,
-                mass: 6,
-                width: 2,
-                height: 2,
-                depth: 400,
-                price: 600,
-                destination: "earth"
-            },
-            {
-                orderId: 2,
-                userId: "Jan jansens",
-                rocketId: 666664,
-                statusId: 2,
-                mass: 2,
-                width: 2,
-                height: 2,
-                depth: 400,
-                price: 200,
-                destination: "mars"
-            },
-            {
-                orderId: 3,
-                userId: "Jan jansens",
-                rocketId: 754874,
-                statusId: 1,
-                mass: 2,
-                width: 2,
-                height: 2,
-                depth: 400,
-                price: 600,
-                destination: "earth"
-            }
-        ];
+const MOCK_ORDERS = [
+    {
+        orderId: "1",
+        userId: "Jan jansens",
+        rocketId: 456464,
+        statusId: 4,
+        mass: 6,
+        width: 2,
+        height: 2,
+        depth: 400,
+        price: 600,
+        destination: "earth"
+    },
+    {
+        orderId: 2,
+        userId: "Jan jansens",
+        rocketId: 666664,
+        statusId: 2,
+        mass: 2,
+        width: 2,
+        height: 2,
+        depth: 400,
+        price: 200,
+        destination: "mars"
+    },
+    {
+        orderId: 3,
+        userId: "Jan jansens",
+        rocketId: 754874,
+        statusId: 1,
+        mass: 2,
+        width: 2,
+        height: 2,
+        depth: 400,
+        price: 600,
+        destination: "earth"
+    },
+    {
+        orderId: 4,
+        userId: "Jan jansens",
+        rocketId: 754874,
+        statusId: 1,
+        mass: 2,
+        width: 2,
+        height: 2,
+        depth: 400,
+        price: 600,
+        destination: "mars"
+    }
+];
 
 function getOrderMock() {
     return {
--- src/assets/js/userOrder.js
+++ src/assets/js/userOrder.js
@@ -1,85 +1,58 @@
 document.addEventListener("DOMContentLoaded", userOrderInit);
-const tableHeaders = "<tr class=\"tableHeaders\"><th scope=\"col\">From:</th><th scope=\"col\">To:</th><th scope=\"col\">Address:</th><th scope=\"col\">Ordernr:</th><th scope=\"col\">Orderstatus:</th><th scope=\"col\">Departure:</th><th scope=\"col\">Arrival:</th><th scope=\"col\">Price:</th><td></td></tr>";
+const tableHeaders = `<tr class=\"tableHeaders\">
+                        <th scope="col">From:</th>
+                        <th scope="col">To:</th>
+                        <th scope="col">Address:</th>
+                        <th scope="col">Ordernr:</th>
+                        <th scope="col">Orderstatus:</th>
+                        <th scope="col">Departure:</th>
+                        <th scope="col">Arrival:</th>
+                        <th scope="col">Price:</th>
+                        <td></td>
+                    </tr>`;
 
 function userOrderInit(){
-    loadInOrders();
+    showOrders();
 }
 
-function loadInOrders() {
-    showOrders("earth");
+function getOrdersByUser(){
+    return apiCall(`users/${getToken()}/orders`, "GET"); // this won't even be necessary if we add a "destination" field to an order
 }
 
-function getOrdersByPlanet(planet){
-    return apiCall(`orders/${planet}`, "GET"); // for now we will use the MOCK_DATA in the respective file
-}
-
-function showOrders(toPlanet /*earth or mars*/){
-    // let orders = getOrdersByPlanet(toPlanet); // api call that filters on the planet you pass with it --> let's use mock data first
-    let orders = MOCK_DATA;
+function showOrders(){
+    // let orders = getOrdersByUser(); // via api call
+    let orders = MOCK_ORDERS;
     let containerEarth = document.querySelector("#flightsToEarthContent");
     let containerMars = document.querySelector("#flightsToMarsContent");
 
-    containerEarth.innerHTML = tableHeaders;
-    containerMars.innerHTML = tableHeaders;
+    let earthOrders = "";
+    let marsOrders = "";
 
-    for (let i = 0; i < orders.length; i++){ // same as before but built with template literals
-        if(orders[i].destination === "earth"){
-            fillTableWithContent(containerEarth, orders[i]);
-        }else if(orders[i].destination === "mars"){
-            fillTableWithContent(containerMars, orders[i]);
+    for (let i = 0; i < orders.length; i++){
+        let order = orders[i];
+
+        if(order.destination === "earth"){
+            earthOrders = fillTableWithContent(earthOrders, order);
+        }else if(order.destination === "mars"){
+            marsOrders = fillTableWithContent(marsOrders, order);
+        }
     }
+
+    containerEarth.innerHTML = tableHeaders + earthOrders;
+    containerMars.innerHTML = tableHeaders + marsOrders;
 }
 
 function fillTableWithContent(container, order){
-    container.innerHTML +=`<tr data-row='" + orders[i].orderId + "'>
-                                    <td>${order.userId}</td>
-                                    <td>NAME RECIEVER</td>
-                                    <td>ADDRESS RECIEVER</td>
-                                    <td>${order.orderId}</td>
-                                    <td>${order.statusId}</td>
-                                    <td>${order.rocketId}</td>
-                                    <td>${order.rocketId}</td>
-                                    <td>${order.price}</td>
-                                    <td><button>view more</button></td>
-                                 </tr>`;
+    container +=`<tr data-row='${order.orderId}'>
+                    <td>${order.userId}</td>
+                    <td>NAME RECIEVER</td>
+                    <td>ADDRESS RECIEVER</td>
+                    <td>${order.orderId}</td>
+                    <td>${order.statusId}</td>
+                    <td>${order.rocketId}</td>
+                    <td>${order.rocketId}</td>
+                    <td>${order.price}</td>
+                    <td><button>view more</button></td>
+                 </tr>`;
+    return container;
 }
-}
-
-/*function showOrdersToEarth(orders) {
-    let orderRowsToEarth ="";
-    for(const i in orders){
-        orderRowsToEarth += "<tr data-row='" + orders[i].orderId + "'>" +
-                                "<td>"+orders[i].userId+"</td>" +
-                                "<td>NAME RECIEVER</td>" +
-                                "<td>ADDRESS RECIEVER</td>" +
-                                "<td>"+orders[i].orderId+"</td>" +
-                                "<td>"+orders[i].statusId+"</td>" +
-                                "<td>"+orders[i].rocketId+"</td>" +
-                                "<td>"+orders[i].rocketId+"</td>" +
-                                "<td>"+orders[i].price+"</td>" +
-                                "<td><button>view more</button></td>" +
-                            "</tr>";
-        console.log(orderRowsToEarth);
-    }
-    document.getElementById("flightsToEarthContent").innerHTML = tableHeaders + orderRowsToEarth;
-}
-
-function showOrdersToMars(orders){
-    let orderRowsToMars="";
-    for(const i in orders){
-        orderRowsToMars += "<tr data-row='" + orders[i].orderId + "'>" +
-                                "<td>"+orders[i].userId+"</td>" +
-                                "<td>NAME RECIEVER</td>" +
-                                "<td>ADDRESS RECIEVER</td>" +
-                                "<td>"+orders[i].orderId+"</td>" +
-                                "<td>"+orders[i].statusId+"</td>" +
-                                "<td>"+orders[i].rocketId+"</td>" +
-                                "<td>"+orders[i].rocketId+"</td>" +
-                                "<td>"+orders[i].price+"</td>" +
-                                "<td><button>view more</button></td>" +
-                            "</tr>";
-        console.log(orderRowsToMars);
-    }
-    document.getElementById("flightsToMarsContent").innerHTML = tableHeaders + orderRowsToMars;
-}
-*/
