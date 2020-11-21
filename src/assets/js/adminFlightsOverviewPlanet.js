b21a2fefe038477c12d0f9e924ec1fa535a55a3a
aronb <aron.buffel@student.howest.be>
2020-11-21 20:06:49 +0100

    commit 6 load in mock data flight overview planet admin

--- src/assets/js/adminFlightsOverviewPlanet.js
+++ src/assets/js/adminFlightsOverviewPlanet.js
@@ -12,4 +12,33 @@ function adminFlightOverviewPlanetInit() {
 }
 
 function showOverview(planet) {
+    const flights = MOCK_FLIGHTS;
+    const container = document.querySelector("#flightsContent");
+
+    let flightsToChosenPlanet = "";
+
+    for (let i = 0; i < flights.length; i++){
+        const FLIGHT = flights[i];
+
+        if(FLIGHT.destination === planet){
+            flightsToChosenPlanet = fillTableWithContent(flightsToChosenPlanet, FLIGHT);
+        }
+    }
+
+    container.innerHTML = tableHeader + flightsToChosenPlanet;
+}
+
+function fillTableWithContent(container, flight){
+    let orders = container;
+    orders +=`<tr data-row='${flight.rocketId}'>
+                    <td>${flight.rocketId}</td>
+                    <td>STATUS</td>
+                    <td>${flight.departure}</td>
+                    <td>${flight.arrival}</td>
+                    <td>${flight.maxMass-flight.availableMass}/${flight.maxMass} kg</td>
+                    <td>${flight.maxVolume-flight.availableVolume}/${flight.maxVolume} m3</td>
+                    <td><button>more info</button></td>
+                 </tr>`;
+    return orders;
+
 }
