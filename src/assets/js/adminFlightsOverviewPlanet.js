307d2c0910ec808fbe87783fb3ae32e7d1eb6a7a
aronb <aron.buffel@student.howest.be>
2020-11-21 20:32:33 +0100

    commit 8 show icon and title selcted planet + small tableheader fix overview and planet overview

--- src/assets/js/adminFlightsOverviewPlanet.js
+++ src/assets/js/adminFlightsOverviewPlanet.js
@@ -1,12 +1,13 @@
 "use strict";
 document.addEventListener("DOMContentLoaded", adminFlightOverviewPlanetInit);
-const tableHeader = '<tr class=\"tableHeaders\"><tr class="tableHeaders"><th scope="col">Flightnr:</th><th scope="col">Orderstatus:</th><th scope="col">Departure:</th><th scope="col">Arrival:</th><th scope="col">Cargo:</th><th scope="col">Volume:</th><td></td></tr>';
+const tableHeader = '<tr class="tableHeaders"><th scope="col">Flightnr:</th><th scope="col">Orderstatus:</th><th scope="col">Departure:</th><th scope="col">Arrival:</th><th scope="col">Cargo:</th><th scope="col">Volume:</th><td></td></tr>';
 
 function adminFlightOverviewPlanetInit() {
     let planet = getDestinationPlanet();
     if(planet!=="mars"&&planet!=="earth"){
         window.location.href = "adminFlightsOverview.html";
     }else{
+        showTitle(planet);
         showOverview(planet);
     }
 }
@@ -40,5 +41,12 @@ function fillTableWithContent(container, flight){
                     <td><button>more info</button></td>
                  </tr>`;
     return orders;
+
+}
+
+
+function showTitle(planet) {
+    const container = document.querySelector("#title");
+    container.innerHTML = '<img src="assets/images/icons/'+planet+'.png" alt="planet icon"><h1>Flights to '+planet+'</h1>';
 
 }
