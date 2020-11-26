0970367680c5b2bb9e33ea6f54b049627e1d7081
Thomas <thomas.van.nevel@student.howest.be>
2020-11-26 13:04:22 +0100

    render in flights

--- src/assets/js/flights.js
+++ src/assets/js/flights.js
@@ -13,6 +13,7 @@ function renderChosenPlanet() {
     CONTAINER.innerHTML =
         `<img src="assets/images/icons/${getDestinationPlanet()}.png" alt="planet icon">
          <h1>Select flight to ${toTitleCase(getDestinationPlanet())}</h1>`;
+    renderRockets();
 }
 
 function toTitleCase(txt){
@@ -20,10 +21,37 @@ function toTitleCase(txt){
 }
 
 
-function renderFlightlist() {
-    getFlights().then(function(flights){
-        for (let i = 0; i < flights.length; i++) {
-            console.log(flight[i]);
+function renderRockets() {
+    const CONTAINER = document.querySelector("#flights table");
+    renderFlightHead(CONTAINER);
+
+    getRockets().then(function(rockets){
+        for (let i = 0; i < rockets.length; i++) {
+            let rocket = rockets[i];
+
+            if (rocket.departLocation === toTitleCase(getDestinationPlanet())){
+                CONTAINER.innerHTML +=
+                    `<tr data-row="${rocket.id}">
+                        <td>${rocket.departure}</td>
+                        <td>${rocket.arrival}</td>
+                        <td>${rocket.availableVolume}</td>
+                        <td>${rocket.availableMass}³</td>
+                        <td>${rocket.pricePerKg} Euro/kg</td>
+                        <td><button>view more</button></td>
+                    </tr>`;
+            }
         }
     });
+}
+
+
+function renderFlightHead(container){
+    container.innerHTML = `<tr id="tableHeaders">
+            <th scope="col">Departure:</th>
+            <th scope="col">Arrival:</th>
+            <th scope="col">Available cargo:</th>
+            <th scope="col">Available volume:</th>
+            <th scope="col">Price:</th>
+            <td></td>
+          </tr>`;
 }
