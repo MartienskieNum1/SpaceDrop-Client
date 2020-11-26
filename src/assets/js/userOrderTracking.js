dfd04b2bd257ab54da5b7870d2689555804da499
aronb <aron.buffel@student.howest.be>
2020-11-26 09:21:39 +0100

    commit 3: user order tracking

--- src/assets/js/userOrderTracking.js
+++ src/assets/js/userOrderTracking.js
@@ -13,23 +13,12 @@ function showOrderdetails() {
 
 function showProgression(progressionLevel) {
     for(let i=1;i<=5;i++){
-        document.getElementById(i.toString()).classList.remove("active");
-    }
-
-    if(progressionLevel>=1){
-        document.getElementById("1").classList.add("active");
-    }
-    if(progressionLevel>=2){
-        document.getElementById("2").classList.add("active");
-    }
-    if(progressionLevel>=3){
-        document.getElementById("3").classList.add("active");
-    }
-    if(progressionLevel>=4){
-        document.getElementById("4").classList.add("active");
-    }
-    if(progressionLevel>=5){
-        document.getElementById("5").classList.add("active");
+        if(progressionLevel>=i){
+            document.getElementById("1").classList.add("active");
+        }
+        else {
+            document.getElementById(i.toString()).classList.remove("active");
+        }
     }
 }
 
