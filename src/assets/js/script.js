3e7f60f8d343c2eb7a4c40e6cd1b88efa7b6cab2
Thomas <thomas.van.nevel@student.howest.be>
2020-12-14 21:56:17 +0100

    temporarily put loading screen on index page

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -4,15 +4,18 @@ onApiUrlLoaded(homeInit);
 
 
 function homeInit(){
+    setTimeout(showNextPage,2000);
     document.querySelector("div#buttons").addEventListener("click", goToFlights);
 }
 
 function goToFlights(e) {
     const ID = e.target.closest("div").getAttribute("id");
-
     addPlanetToLocalStorage(ID);
+}
 
-    window.location.href = "flights.html";
+function showNextPage(){
+    document.getElementById("loader").style.display = "none";
+    document.getElementById("buttons").style.display = "flex";
 }
 
 function addPlanetToLocalStorage(id) {
