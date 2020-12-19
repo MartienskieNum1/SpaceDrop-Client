d54f400d0f8a80d083411d31129cc058fbcd4a6f
aronb <aron.buffel@student.howest.be>
2020-12-19 12:50:46 +0100

    commit 5 fix past orders users

--- src/assets/js/userOrderNew.js
+++ src/assets/js/userOrderNew.js
@@ -2,12 +2,13 @@ function showRockets(rockets, orders,tableHeaders) {
     let earthOrders = tableHeaders;
     let marsOrders = tableHeaders;
     for (let x = 0; x < rockets.length; x++){
-        for(let y = 0; y < orders.length; y++)
-            if(orders[y].rocketId.toString()===rockets[x].id.toString() && rockets[x].departLocation === "Mars" && orders[y].statusId<3) {
+        for(let y = 0; y < orders.length; y++) {
+            if (orders[y].rocketId.toString() === rockets[x].id.toString() && rockets[x].departLocation === "Mars" && orders[y].statusId < 3) {
                 earthOrders += fillTableWithContent(earthOrders, orders[y], rockets[x]);
-            }else if(orders[y].rocketId.toString()===rockets[x].id.toString() && rockets[x].departLocation === "Earth"&& orders[y].statusId<3) {
+            } else if (orders[y].rocketId.toString() === rockets[x].id.toString() && rockets[x].departLocation === "Earth" && orders[y].statusId < 3) {
                 marsOrders += fillTableWithContent(marsOrders, orders[y], rockets[x]);
             }
+        }
     }
     containerEarth.innerHTML = earthOrders;
     containerMars.innerHTML = marsOrders;
